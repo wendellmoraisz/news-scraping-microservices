9614d6312f80e2898782f31ba85969b1536d0d4b
wendellmoraisz <wendellmorais3319@gmail.com>
2023-11-23 10:24:32 -0300

    insert email in repository

--- EmailManager/EmailManager.Infrastructure/Repositories/EmailRepository.cs
+++ EmailManager/EmailManager.Infrastructure/Repositories/EmailRepository.cs
@@ -8,17 +8,24 @@ namespace EmailManager.Infrastructure.Repositories;
 public class EmailRepository : IEmailRepository
 {
     private readonly EmailContext _context;
-    
+
     public EmailRepository(EmailContext context)
     {
         _context = context;
     }
-    
-    public async Task<Email?> GetByAddress(string emailAddress)
+
+    public async Task<Email?> GetByAddress(string emailAddress, CancellationToken cancellationToken)
     {
         return await _context
             .Emails
             .Find(email => email.Address == emailAddress)
-            .FirstOrDefaultAsync();
+            .FirstOrDefaultAsync(cancellationToken);
     }
+    
+    public async Task<Email> CreateEmail(Email email, CancellationToken cancellationToken)
+    {
+        await _context.Emails.InsertOneAsync(email, cancellationToken);
+        return email;
+    }
+    
 }
