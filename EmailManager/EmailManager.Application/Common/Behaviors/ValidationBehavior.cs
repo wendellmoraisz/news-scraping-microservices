54347ade0f3fe7a7760253ce6f0730b7a677ab54
wendellmoraisz <wendellmorais3319@gmail.com>
2023-11-23 09:26:15 -0300

    add validation behavior in EmailManager service

---
+++ EmailManager/EmailManager.Application/Common/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,38 @@
+using FluentValidation;
+using MediatR;
+using EmailManager.Application.Common.Exceptions;
+
+namespace EmailManager.Application.Common.Behaviors;
+
+public sealed class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+    {
+        _validators = validators;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (!_validators.Any()) return await next();
+        
+        var context = new ValidationContext<TRequest>(request);
+        var validationTasks = _validators.Select(x => x.ValidateAsync(context, cancellationToken)).ToList();
+
+        var errors = validationTasks
+            .SelectMany(task => task.Result.Errors)
+            .Where(error => error != null)
+            .Select(error => error)
+            .Distinct()
+            .ToArray();
+
+        var errorsCodes = errors.Select(e => e.ErrorCode).ToArray();
+        var errorsMessages = errors.Select(e => e.ErrorMessage).ToArray();
+        
+        if (errorsCodes.Contains("409")) throw new ConflictException(errorsMessages);
+        if (errors.Any()) throw new BadRequestException(errorsMessages);
+        return await next();
+    }
+}
