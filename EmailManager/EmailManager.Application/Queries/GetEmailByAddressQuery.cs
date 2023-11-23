0f523daa62d1ba2a5184bb5f60d55e51c23855f5
wendellmoraisz <wendellmorais3319@gmail.com>
2023-11-23 09:15:28 -0300

    refactor: using usecases pattern

--- EmailManager/EmailManager.Application/Queries/GetEmailByAddressQuery.cs
+++
@@ -1,9 +0,0 @@
-using EmailManager.Application.Responses;
-using MediatR;
-
-namespace EmailManager.Application.Queries;
-
-public class GetEmailByAddressQuery : IRequest<EmailResponse>
-{
-    public string Address { get; set; }
-}
--- EmailManager/EmailManager.Application/Responses/EmailResponse.cs
+++
@@ -1,7 +0,0 @@
-namespace EmailManager.Application.Responses;
-
-public class EmailResponse
-{
-    public string Id { get; set; }
-    public string Address { get; set; }
-}
