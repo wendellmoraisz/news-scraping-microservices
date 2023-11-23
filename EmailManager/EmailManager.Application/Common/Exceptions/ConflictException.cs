f61da3e72a5ab9279ae61fb7601ac03d47135d32
wendellmoraisz <wendellmorais3319@gmail.com>
2023-11-23 09:25:37 -0300

    add exceptions in emailManager service

---
+++ EmailManager/EmailManager.Application/Common/Exceptions/ConflictException.cs
@@ -0,0 +1,12 @@
+namespace EmailManager.Application.Common.Exceptions;
+
+public class ConflictException : Exception
+{
+    public ConflictException(string error) : base(error)
+    {
+    }
+    
+    public ConflictException(string[] errors) : base(string.Join(";", errors))
+    {
+    }
+}
---
+++ EmailManager/EmailManager.Application/Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,8 @@
+namespace EmailManager.Application.Common.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException(string message) : base(message)
+    {
+    }
+}
