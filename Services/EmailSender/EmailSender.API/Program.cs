86b840dd7ae5e1e324b68d674e4301494b4c39fe
wendellmoraisz <wendellmorais3319@gmail.com>
2023-11-30 20:52:33 -0300

    build: config docker in email sender api

--- Services/EmailSender/EmailSender.API/Program.cs
+++ Services/EmailSender/EmailSender.API/Program.cs
@@ -1,4 +1,12 @@
+using EmailSender.Application.Services;
+using EmailSender.Core.Entities;
+using EmailSender.Infrastructure.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<ISendEmailService, SendEmailService>();
+builder.Services.Configure<SmtpOptions>(builder.Configuration.GetSection("SmtpOptions"));
+
 var app = builder.Build();
 
 app.Run();
