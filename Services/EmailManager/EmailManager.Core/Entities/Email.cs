d73129606f45da66b374ef4f7a862963f1266be4
wendellmoraisz <wendellmorais3319@gmail.com>
2023-12-12 16:58:29 -0300

    raw: config email address as mongo property

--- Services/EmailManager/EmailManager.Core/Entities/Email.cs
+++ Services/EmailManager/EmailManager.Core/Entities/Email.cs
@@ -1,6 +1,9 @@
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace EmailManager.Core.Entities;
 
 public class Email : BaseEntity
 {
+    [BsonElement("Address")]
     public string Address { get; set; }
 }
